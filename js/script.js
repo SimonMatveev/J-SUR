1670d39fa737c9677fce84b74a5fb52da41079e9
SimonMatveev <the.unknown.guest@yandex.com>
2022-07-29 13:38:52 +0300

    menu opening filter

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ new fullpage('#fullpage', {
 let burger = document.querySelector(".header__burger");
 let menu = document.querySelector(".header__menu");
 let body = document.querySelector(".body");
+let firstScreen = document.querySelector(".first-screen");
 
 if (burger) {
 	burger.onclick = function () {
@@ -18,15 +19,17 @@ if (burger) {
 			menu.classList.toggle("header__menu--active");
 			burger.classList.toggle("header__burger--active");
 			fullpage_api.setAllowScrolling(false, 'down, up');
+			firstScreen.style.filter = 'brightness(50%)';
 		}
 	}
 }
 
 window.addEventListener('click', function (e) {
 	if (!menu.contains(e.target) && !burger.contains(e.target) && (menu.classList.contains("header__menu--active"))) {
-		menu.classList.toggle("header__menu--active");
-		burger.classList.toggle("header__burger--active");
+		menu.classList.remove("header__menu--active");
+		burger.classList.remove("header__burger--active");
 		fullpage_api.setAllowScrolling(true, 'down, up');
+		firstScreen.style.filter = 'brightness(100%)';
 	}
 })
 
@@ -35,38 +38,37 @@ window.addEventListener('click', function (e) {
 let firstScreenPrev = document.querySelector(".first-screen__button-prev");
 let firstScreenNext = document.querySelector(".first-screen__button-next");
 let advertisments = document.querySelectorAll(".first-screen__advertisment");
-let first_screen = document.querySelector(".first-screen");
 let headerLogo = document.querySelector(".header__logo");
 let circle = document.querySelector(".first-screen__circle");
 
 advertisments[0].classList.add("first-screen__advertisment--active");
 advertisments[0].classList.add("first-screen__advertisment--opacity");
-let current_active = 0;
+let currentActive = 0;
 let cycle = 0;
 
 firstScreenNext.onclick = function () {
 	for (let i = 0; i < advertisments.length; i++) {
 		if (advertisments[i].classList.contains("first-screen__advertisment--active")) {
-			current_active = i;
+			currentActive = i;
 		}
 	}
-	if (advertisments[current_active + 1]) {
-		advertisments[current_active + 1].classList.add("first-screen__advertisment--active");
-		advertisments[current_active].classList.remove("first-screen__advertisment--active");
+	if (advertisments[currentActive + 1]) {
+		advertisments[currentActive + 1].classList.add("first-screen__advertisment--active");
+		advertisments[currentActive].classList.remove("first-screen__advertisment--active");
 		setTimeout(() => {
-			advertisments[current_active + 1].classList.add("first-screen__advertisment--opacity");
-			advertisments[current_active].classList.remove("first-screen__advertisment--opacity");
+			advertisments[currentActive + 1].classList.add("first-screen__advertisment--opacity");
+			advertisments[currentActive].classList.remove("first-screen__advertisment--opacity");
 		}, 5);
 	} else {
 		advertisments[0].classList.add("first-screen__advertisment--active");
-		advertisments[current_active].classList.remove("first-screen__advertisment--active");
+		advertisments[currentActive].classList.remove("first-screen__advertisment--active");
 		setTimeout(() => {
 			advertisments[0].classList.add("first-screen__advertisment--opacity");
-			advertisments[current_active].classList.remove("first-screen__advertisment--opacity");
+			advertisments[currentActive].classList.remove("first-screen__advertisment--opacity");
 		}, 5);
 	}
 	const screenWidth = window.screen.width;
-	if (current_active != 2 && screenWidth > 575) {
+	if (currentActive != 2 && screenWidth > 575) {
 		headerLogo.style.color = 'black';
 	} else {
 		headerLogo.style.color = 'white';
@@ -78,26 +80,26 @@ firstScreenNext.onclick = function () {
 firstScreenPrev.onclick = function () {
 	for (let i = 0; i < advertisments.length; i++) {
 		if (advertisments[i].classList.contains("first-screen__advertisment--active")) {
-			current_active = i;
+			currentActive = i;
 		}
 	}
-	if (advertisments[current_active - 1]) {
-		advertisments[current_active - 1].classList.add("first-screen__advertisment--active");
-		advertisments[current_active].classList.remove("first-screen__advertisment--active");
+	if (advertisments[currentActive - 1]) {
+		advertisments[currentActive - 1].classList.add("first-screen__advertisment--active");
+		advertisments[currentActive].classList.remove("first-screen__advertisment--active");
 		setTimeout(() => {
-			advertisments[current_active - 1].classList.add("first-screen__advertisment--opacity");
-			advertisments[current_active].classList.remove("first-screen__advertisment--opacity");
+			advertisments[currentActive - 1].classList.add("first-screen__advertisment--opacity");
+			advertisments[currentActive].classList.remove("first-screen__advertisment--opacity");
 		}, 5);
 	} else {
 		advertisments[advertisments.length - 1].classList.add("first-screen__advertisment--active");
-		advertisments[current_active].classList.remove("first-screen__advertisment--active");
+		advertisments[currentActive].classList.remove("first-screen__advertisment--active");
 		setTimeout(() => {
 			advertisments[advertisments.length - 1].classList.add("first-screen__advertisment--opacity");
-			advertisments[current_active].classList.remove("first-screen__advertisment--opacity");
+			advertisments[currentActive].classList.remove("first-screen__advertisment--opacity");
 		}, 5);
 	}
 	const screenWidth = window.screen.width;
-	if (current_active != 1 && screenWidth > 575) {
+	if (currentActive != 1 && screenWidth > 575) {
 		headerLogo.style.color = 'black';
 	} else {
 		headerLogo.style.color = 'white';
